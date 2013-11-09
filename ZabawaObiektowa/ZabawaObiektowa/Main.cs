b9959b43615b9f960a15a5fe273c6f3e4f7809c9
Michał Konieczny <mic22@mic22.info>
2013-11-09 16:51:19 +0100

    przykładowa klasa BankAccount

--- ZabawaObiektowa/ZabawaObiektowa/Main.cs
+++ ZabawaObiektowa/ZabawaObiektowa/Main.cs
@@ -1,12 +1,42 @@
 using System;
 
 namespace ZabawaObiektowa
-{
+{	
+	class BankAccount
+	{
+		public BankAccount (decimal amount)
+		{
+			balance=amount;
+		}
+		public void withdrawal (decimal amount)
+		{
+			balance-=amount;
+		}
+		public void deposit (decimal amount)
+		{
+			balance+=amount;
+		}
+		public decimal getBalance ()
+		{
+			return balance;
+		}
+		public string getOwner ()
+		{
+			return owner;
+		}
+
+		private decimal balance;
+		private string	owner;
+	}
+
+
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			BankAccount account=new BankAccount(1000);
+			account.withdrawal(100);
+			Console.WriteLine(account.getBalance());
 		}
 	}
 }
