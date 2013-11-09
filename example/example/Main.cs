966c246db0d4335ad4641f5e48b8771a67f1493d
Michał Konieczny <mic22@mic22.info>
2013-11-09 16:35:58 +0100

    dodanie przykladowego projektu

---
+++ example/example/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_z_tablic
+{
+    class Program
+    {
+        static void Main (string[] args)
+		{
+			foreach (var item in args) {
+				Console.WriteLine (item);
+			};
+        }
+    }
+}
